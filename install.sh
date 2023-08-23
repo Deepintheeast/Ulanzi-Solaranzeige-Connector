db7228ec36f11ba7b7a8f8bb0c2986130207de1f
Deepintheeast <nospam@biglinux.de>
2023-08-23 17:02:33 +0200

    PreRelease v0.4

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-apt update
-apt dist-upgrade -y
-apt install pip
-pip3 install logging
-pip3 install bs4
-pip3 install lxml
+#apt update
+#apt dist-upgrade -y
+#apt install pip
+#pip3 install logging
+#pip3 install bs4
+#pip3 install lxml
 
 mkdir /home/pi/scripts
 cd /home/pi/scripts
 
 git clone https://github.com/Deepintheeast/Ulanzi-Solaranzeige-Connector
+
+mv Ulanzi-Solaranzeige-Connector Ulanzi-Solaranzeige-Connector-$1
+cd /home/pi/scripts/Ulanzi-Solaranzeige-Connector-$1
+
+sed -i 's/Ulanzi-Solaranzeige-Connector/Ulanzi-Solaranzeige-Connector-$1/g' ulanzi-connector.service
+mv ulanzi-connector.service ulanzi-connector-$1.service
+
